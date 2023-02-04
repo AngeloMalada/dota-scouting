c7d1fa8916c380f1c8459b1d61135da9d02692fa
angelomalada <angelo.malada@gmail.com>
2023-02-04 21:33:06 +0100

    added bottom padding

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,7 +3,7 @@ import Navbar from './Navbar';
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const Layout = ({ children }: any) => {
   return (
-    <div className='min-h-screen bg-[#151515] px-10 text-white'>
+    <div className='min-h-screen bg-[#151515] px-10 text-white pb-20'>
       <Navbar />
       {children}
     </div>
