94549b62353cc0c617d41dce09138f17594880f7
angelomalada <angelo.malada@gmail.com>
2023-02-04 21:05:56 +0100

    added div 1 team data

--- Teams.js
+++ Teams.js
@@ -5,74 +5,193 @@ const Teams = {
       id: 1,
       teams: [
         {
-          name: 'Panda',
+          name: 'Team vanilla',
           id: 1,
           players: [
             {
-              name: 'panda for president',
+              name: 'vanilla',
               id: 1,
-              accountID: 72407726,
+              accountID: 195945156,
             },
             {
-              name: 'panda for president',
+              name: 'nfd',
               id: 2,
-              accountID: 104279179,
+              accountID: 107696053,
             },
             {
-              name: 'panda for president',
+              name: 'midking)',
               id: 3,
-              accountID: 907589158,
+              accountID: 235880461,
             },
             {
-              name: 'panda for president',
+              name: 'valakiiiiiiiiii',
               id: 4,
-              accountID: 170147156,
+              accountID: 135425789,
             },
             {
-              name: 'panda for president',
+              name: 'Gecko',
               id: 5,
-              accountID: 839580781,
-            },
-            {
-              name: 'panda for president',
-              id: 6,
-              accountID: 839580781,
-            },
-            {
-              name: 'panda for president',
-              id: 7,
-              accountID: 839580781,
+              accountID: 146060489,
             },
           ],
         },
+
         {
-          name: 'Panda is cool',
+          name: 'Team SoLo',
           id: 2,
           players: [
             {
-              name: 'panda for president',
+              name: 'SoLo',
               id: 1,
-              accountID: 72407726,
+              accountID: 309314822,
             },
             {
-              name: 'panda for president',
+              name: 'Jammy',
               id: 2,
-              accountID: 104279179,
+              accountID: 113805244,
             },
             {
-              name: 'panda for president',
+              name: 'Kenshi',
               id: 3,
-              accountID: 907589158,
+              accountID: 81345482,
             },
             {
-              name: 'panda for president',
+              name: 'jihapse',
               id: 4,
-              accountID: 170147156,
+              accountID: 110881046,
             },
             {
-              name: 'panda for president',
+              name: 'Calli',
               id: 5,
-              accountID: 839580781,
+              accountID: 125155223,
+            },
+          ],
+        },
+
+        {
+          name: 'Team Bonjwa',
+          id: 3,
+          players: [
+            {
+              name: 'Bonjwa',
+              id: 1,
+              accountID: 81952474,
+            },
+            {
+              name: 'Cvaekt',
+              id: 2,
+              accountID: 287978960,
+            },
+            {
+              name: 'Dredi',
+              id: 3,
+              accountID: 51973692,
+            },
+            {
+              name: 'play',
+              id: 4,
+              accountID: 113312766,
+            },
+            {
+              name: 'Mango',
+              id: 5,
+              accountID: 167275877,
+            },
+          ],
+        },
+
+        {
+          name: 'Team Knaryaa',
+          id: 4,
+          players: [
+            {
+              name: 'Knaryaa',
+              id: 1,
+              accountID: 111557371,
+            },
+            {
+              name: 'Frau',
+              id: 2,
+              accountID: 172239333,
+            },
+            {
+              name: 'Stylezz',
+              id: 3,
+              accountID: 91516368,
+            },
+            {
+              name: 'sukui',
+              id: 4,
+              accountID: 1182296166,
+            },
+            {
+              name: 'TuEg',
+              id: 5,
+              accountID: 174026441,
+            },
+          ],
+        },
+
+        {
+          name: 'Team ImBaSaRgArEpA',
+          id: 5,
+          players: [
+            {
+              name: 'ImBaSaRgArEpA',
+              id: 1,
+              accountID: 86719611,
+            },
+            {
+              name: 'Maff',
+              id: 2,
+              accountID: 40916046,
+            },
+            {
+              name: 'Neox',
+              id: 3,
+              accountID: 26773166,
+            },
+            {
+              name: 'f',
+              id: 4,
+              accountID: 184601350,
+            },
+            {
+              name: 'Krastavac',
+              id: 5,
+              accountID: 314442736,
+            },
+          ],
+        },
+
+        {
+          name: 'Team legitsplit',
+          id: 6,
+          players: [
+            {
+              name: 'legitsplit',
+              id: 1,
+              accountID: 84075077,
+            },
+            {
+              name: 'Draagaan',
+              id: 2,
+              accountID: 451589360,
+            },
+            {
+              name: 'WHONOOBNOW',
+              id: 3,
+              accountID: 75376818,
+            },
+            {
+              name: 'KTZ',
+              id: 4,
+              accountID: 119695137,
+            },
+            {
+              name: 'Indie',
+              id: 5,
+              accountID: 123988641,
             },
           ],
         },
